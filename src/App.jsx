ed698440950cdc9265fe67409d5efe02e2c71f2c
Priya Pradhan <priy437pradhan@gmail.com>
2024-04-19 15:29:04 +0530

    Resolved conflicts

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+import 'typeface-roboto';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default App;
